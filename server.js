26efa93d5984bf92847736067ada0f0449b8b7dc
marcelo silvero <marcesilvero@gmail.com>
2020-05-01 21:25:45 -0400

    mi primer commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;  // agregar puerto para produccion || puerto local
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Andrea Carolina \u2764'
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about');
+});
+
+app.listen(port, () => {
+    console.log(`Escuchando peticiones en el puerto ${ port }`);
+});
